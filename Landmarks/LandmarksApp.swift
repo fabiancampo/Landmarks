b45052982109cd2a14f4ae477ea8b0c1939be6b2
Fabián Gómez Campo <fabian.gomez@usal.es>
2024-01-20 19:42:54 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Fabián Gómez Campo on 20/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
